580b85635500a6c11f2a353bdd5ebb261bf4f183
dampbacon <7982855+dampbacon@users.noreply.github.com>
2022-08-29 17:58:48 +1200

    Moved starter code
    starter code moved from __init__ to home.py blueprint.
    runs again, project finally setup basically so work can be done finally

--- music/__init__.py
+++ music/__init__.py
@@ -4,16 +4,10 @@ from flask import Flask, render_template
 
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
-from music.domainmodel.track import Track
 
 
 # TODO: Access to the tracks should be implemented via the repository pattern and using blueprints, so this can not
 #  stay here!
-def create_some_track():
-    some_track = Track(1, "Heat Waves")
-    some_track.track_duration = 250
-    some_track.track_url = 'https://spotify/track/1'
-    return some_track
 
 
 def create_app():
@@ -27,8 +21,8 @@ def create_app():
     #
     # return app
     with app.app_context():
-        from .home import routes
-        from .tracks_bp import routes
+        from .home import home
+        from .tracks_bp import tracks_bp
 
         app.register_blueprint(home.blueprint_home)
         app.register_blueprint(tracks_bp.blueprint_track)
---
+++ music/home/home.py
@@ -0,0 +1,23 @@
+# home blueprint stuff
+from flask import Blueprint, render_template
+from flask import current_app
+
+from music.domainmodel.track import Track
+
+blueprint_home = Blueprint('home_page', __name__, template_folder='templates', static_folder='static')
+
+
+
+#test code
+def create_some_track():
+    some_track = Track(1, "Heat Waves")
+    some_track.track_duration = 250
+    some_track.track_url = 'https://spotify/track/1'
+    return some_track
+
+
+@blueprint_home.route('/', methods=['GET'])
+def home():
+    some_track = create_some_track()
+    #     # Use Jinja to customize a predefined html page rendering the layout for showing a single track.
+    return render_template('simple_track.html', track=some_track)
