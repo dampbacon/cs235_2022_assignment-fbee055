8ce38704d8bb5f781cc72a5e1033b6558a7d345e
dampbacon <7982855+dampbacon@users.noreply.github.com>
2022-09-06 16:01:35 +1200

    basic object created
    basic object created, needs to be restructured to make attributes private and to create other lists as attributes.
    
    Methods need to be made to work on any list.

--- music/tracks_bp/track_methods.py
+++ music/tracks_bp/track_methods.py
@@ -4,6 +4,7 @@ from music.domainmodel.track import Track
 from music.adapters.csvdatareader import TrackCSVReader
 from multipledispatch import dispatch
 
+
 #
 # INITIALIZE a csv reader object and make it read tracks. This is needed to find the tracks needed to display them
 #
@@ -13,11 +14,6 @@ from multipledispatch import dispatch
 # reader_obj = TrackCSVReader('tests/data/raw_albums_test.csv', 'tests/data/raw_tracks_test.csv')
 #
 # real deal code
-reader_obj = TrackCSVReader('tests/data/raw_albums_excerpt.csv', 'tests/data/raw_tracks_excerpt.csv')
-
-reader_obj.read_csv_files()
-tracks = reader_obj.dataset_of_tracks
-
 def create_some_track():
     some_track = Track(1, "Heat Wavesaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas")
     some_track.track_duration = 250
@@ -25,71 +21,78 @@ def create_some_track():
     return some_track
 
 
-def find_track(track_id):
-    for i in tracks:
-        if i.track_id == track_id:
-            return i, tracks.index(i)
+class csvreader_track_methods_extension:
+    def __init__(self):
+        reader_obj = TrackCSVReader('tests/data/raw_albums_excerpt.csv', 'tests/data/raw_tracks_excerpt.csv')
 
+        reader_obj.read_csv_files()
+        self.tracks = reader_obj.dataset_of_tracks
 
-def get_next_and_previous_track(current_track):
-    temp_list = [0, 0]
-    if current_track[0] == get_last_track():
-        print("WHAT IS WRONG. TRACK PREVIOUS", tracks[current_track[1] - 1])
-        temp_list[0] = tracks[current_track[1] - 1]
-    elif current_track[0] == get_first_track():
-        print("WHAT IS WRONG. TRACK PREVIOUS", tracks[current_track[1] + 1])
-        temp_list[1] = tracks[current_track[1] + 1]
-    elif current_track[0] != get_last_track() and current_track[0] != get_first_track():
-        temp_list = [tracks[current_track[1] - 1], tracks[current_track[1] + 1]]
-    return tuple(temp_list)
+    def find_track(self, track_id):
+        for i in self.tracks:
+            if i.track_id == track_id:
+                return i, self.tracks.index(i)
 
+    def get_next_and_previous_track(self, current_track):
+        temp_list = [0, 0]
+        if current_track[0] == self.get_last_track():
+            print("WHAT IS WRONG. TRACK PREVIOUS", self.tracks[current_track[1] - 1])
+            temp_list[0] = self.tracks[current_track[1] - 1]
+        elif current_track[0] == self.get_first_track():
+            print("WHAT IS WRONG. TRACK PREVIOUS", self.tracks[current_track[1] + 1])
+            temp_list[1] = self.tracks[current_track[1] + 1]
+        elif current_track[0] != self.get_last_track() and current_track[0] != self.get_first_track():
+            temp_list = [self.tracks[current_track[1] - 1], self.tracks[current_track[1] + 1]]
+        return tuple(temp_list)
 
-@dispatch(bool)
-def sort_by_track_name(sort_order_bool=True):
-    tracks.sort(key=lambda i: i.title.upper() if i.title else '', reverse=sort_order_bool)
+    @dispatch(bool)
+    def sort_by_track_name(self, sort_order_bool=True):
+        self.tracks.sort(key=lambda i: i.title.upper() if i.title else '', reverse=sort_order_bool)
 
+    @dispatch(list, bool)
+    def sort_by_track_name(self, tracks_list, sort_order_bool=True):
+        # temp code
+        tracks_list = self.tracks
 
-@dispatch(list, bool)
-def sort_by_track_name(tracks_list=tracks, sort_order_bool=True):
-    tracks_list.sort(key=lambda i: i.title.upper() if i.title else '', reverse=sort_order_bool)
+        tracks_list.sort(key=lambda i: i.title.upper() if i.title else '', reverse=sort_order_bool)
 
+    @dispatch(bool)
+    def sort_by_album_name(self, sort_order_bool=True):
+        self.tracks.sort(key=lambda i: i.album.title.upper() if i.album else '', reverse=sort_order_bool)
 
-@dispatch(bool)
-def sort_by_album_name(sort_order_bool=True):
-    tracks.sort(key=lambda i: i.album.title.upper() if i.album else '', reverse=sort_order_bool)
+    @dispatch(list, bool)
+    def sort_by_album_name(self, tracks_list, sort_order_bool=True):
+        tracks_list = self.tracks
+        tracks_list.sort(key=lambda i: i.album.title.upper() if i.album else '', reverse=sort_order_bool)
 
+    def sort_by_track_id(self):
+        self.tracks.sort()
 
-@dispatch(list, bool)
-def sort_by_album_name(tracks_list=tracks, sort_order_bool=True):
-    tracks_list.sort(key=lambda i: i.album.title.upper() if i.album else '', reverse=sort_order_bool)
+    def get_first_track(self):
+        return self.tracks[0]
 
+    def get_last_track(self):
+        return self.tracks[-1]
 
-def sort_by_track_id():
-    tracks.sort()
+    # list type zero raw data
+    def get_track_data(self, list_type=0):
+        return self.tracks
 
-
-def get_first_track():
-    return tracks[0]
-
-
-def get_last_track():
-    return tracks[-1]
-
-
-def get_track_data():
-    pass
-
-
-def create_bookmarks(by_track_name=0):
-    bookmarks = dict()
-    # the bookmarks are to allow browsing at a bookmark via link, 0= track name 1= album name 2= artist name??
-    if by_track_name == 0:
-        for i in range(len(tracks)):
-            if tracks[i].title is None and 'unnamed' not in bookmarks:
-                bookmarks['unnamed'] = tracks[i].track_id
-            else:
-                if tracks[i].title[0].upper() not in bookmarks:
-                    bookmarks[tracks[i].title[0].upper()] = tracks[i].track_id
+    @dispatch(int)
+    def create_bookmarks(self, by_track_name):
+        bookmarks = None
+        # the bookmarks are to allow browsing at a bookmark via link, 0= track name 1= album name 2= artist name??
+        if by_track_name == 0:
+            bookmarks = dict()
+            for i in range(len(self.tracks)):
+                if self.tracks[i].title is None and 'unnamed' not in bookmarks:
+                    bookmarks['unnamed'] = self.tracks[i].track_id
                 else:
-                    pass
-    return bookmarks.items()
+                    if self.tracks[i].title[0].upper() not in bookmarks:
+                        bookmarks[self.tracks[i].title[0].upper()] = self.tracks[i].track_id
+                    else:
+                        pass
+        try:
+            return bookmarks.items()
+        except AttributeError:
+            return None
--- music/tracks_bp/tracks_bp.py
+++ music/tracks_bp/tracks_bp.py
@@ -7,6 +7,8 @@ from music.tracks_bp.track_methods import *
 
 blueprint_track = Blueprint('tracks_page', __name__, template_folder='templates', static_folder='static')
 
+track_methods = csvreader_track_methods_extension()
+tracks = track_methods.get_track_data()
 
 @blueprint_track.route('/track', methods=['GET'])
 def display_track():
@@ -17,27 +19,28 @@ def display_track():
 
 @blueprint_track.route('/track/<int:track_id>', methods=['get'])
 def display_track_at_id(track_id=None):
-    track_data = find_track(track_id)
+    track_data = track_methods.find_track(track_id)
     # sort_by_track_name()
     # works should be moved later
     # sort_by_album_name()
 
     print("track data:", track_data[0])
-    np_url_id_tuple = get_next_and_previous_track(track_data)
+    np_url_id_tuple = track_methods.get_next_and_previous_track(track_data)
     print('NP_URL_TUPLE_________: ', np_url_id_tuple)
-    return render_template('display_track.html', track=track_data[0], np_tuple=np_url_id_tuple, first=get_first_track(),
-                           last=get_last_track(), bookmarks=create_bookmarks())
+    return render_template('display_track.html', track=track_data[0], np_tuple=np_url_id_tuple,
+                           first=track_methods.get_first_track(),
+                           last=track_methods.get_last_track(), bookmarks=track_methods.create_bookmarks(0))
 
 
 @blueprint_track.route('/track/<int:track_id>/sort_by_album', methods=['get'])
 def sort_by_album_button(track_id=None):
-    sort_by_album_name(False)
+    track_methods.sort_by_album_name(False)
     return redirect(url_for('tracks_page.display_track_at_id', track_id=track_id))
 
 
 @blueprint_track.route('/track/<int:track_id>/sort_by_track_name', methods=['get'])
 def sort_by_track_name_button(track_id=None):
-    sort_by_track_name(False)
-    a = create_bookmarks()
+    track_methods.sort_by_track_name(False)
+    a = track_methods.create_bookmarks(0)
     print('_______________________________________________ BOOKMARKS', a)
     return redirect(url_for('tracks_page.display_track_at_id', track_id=track_id))
