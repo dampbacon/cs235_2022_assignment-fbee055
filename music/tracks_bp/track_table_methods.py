4e942246c82ef46fcdb723c7618a02992035d8ba
dampbacon <7982855+dampbacon@users.noreply.github.com>
2022-09-07 22:37:21 +1200

    implemented tables messily
    title, basically had allot of errors main one that cause me to die was https://datatables.net/manual/tech-notes/4
    
    and i don't really know how to js and ajax .
    
    follow up updates will clean up table and add in links

--- music/tracks_bp/track_table_methods.py
+++ music/tracks_bp/track_table_methods.py
@@ -6,22 +6,36 @@ def album_name(track: Track):
     if track.album is not None:
         return track.album.title
     else:
-        return None
+        return 'None'
 
 
 def artist_name(track: Track):
     if track.artist is not None:
         return track.artist.full_name
     else:
-        return None
+        return 'none'
+
+
+def track_name(track: Track):
+    if track.title is not None:
+        return track.title
+    else:
+        return 'none'
+
+
+def track_duration(track: Track):
+    if track.track_duration is not None:
+        return track.track_duration
+    else:
+        return 0
 
 
 def track_obj_to_dict(track: Track):
     return {
-        'track_id': track.track_id,
-        'track_name': track.title,
-        'duration': track.track_duration,
-        'url': track.track_url,
-        'album': album_name(track),
-        'artist': artist_name(track)
+        'track_id': int(track.track_id),
+        'track_name': str(track_name(track)),
+        'duration': int(track_duration(track)),
+        'url': '/track/' + str(track.track_id),
+        'album': str(album_name(track)),
+        'artist': str(artist_name(track))
     }
