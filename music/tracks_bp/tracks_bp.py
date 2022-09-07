67c6920b0096f514b39b4a5f159b713f23e8159d
dampbacon <7982855+dampbacon@users.noreply.github.com>
2022-09-07 20:27:17 +1200

    genre displayed

--- music/tracks_bp/tracks_bp.py
+++ music/tracks_bp/tracks_bp.py
@@ -45,7 +45,7 @@ def display_track_at_id(track_id=None):
     return render_template('display_track.html', track=track_data[0], np_tuple=np_url_id_tuple,
                            first=track_methods.get_first_track(list_from_query),
                            last=track_methods.get_last_track(list_from_query),
-                           bookmarks=bookmarks, query_params=query_params)
+                           bookmarks=bookmarks, query_params=query_params, genre_list=track_data[0].genres)
 
 
 @blueprint_track.route('/track/<int:track_id>/sort_by_album', methods=['get'])
