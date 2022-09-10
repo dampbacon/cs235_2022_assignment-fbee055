36953ff27fbb17d8b9eb0d5c0a49eaa969a33809
Matthew Young <69370714+Cookiebyted@users.noreply.github.com>
2022-09-10 22:01:06 +1200

    Added active jinja nav + slight splashpage UI change

--- tests/unit/test_domainmodel.py
+++ tests/unit/test_domainmodel.py
@@ -1,12 +1,7 @@
 import pytest
 import os
 
-from music.domainmodel.artist import Artist
-from music.domainmodel.track import Track
-from music.domainmodel.genre import Genre
-from music.domainmodel.review import Review
-from music.domainmodel.album import Album
-from music.domainmodel.user import User
+from music.domainmodel.model import *
 from music.adapters.csvdatareader import TrackCSVReader
 
 
