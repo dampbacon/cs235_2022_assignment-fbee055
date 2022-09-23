f23b1f7b68e2d711b6fbefcbbf2ad5d281b32fc9
Francois Beetge <7982855+dampbacon@users.noreply.github.com>
2022-09-23 22:33:54 +1200

    track search

--- music/search/search_methods.py
+++ music/search/search_methods.py
@@ -20,7 +20,6 @@ def search_by_album_name(repository=repo, string_to_search='free', shallow_searc
 
             break
     print(index, index_end)
-
     realIndex1 = tracks_sorted_by_albums.index(repository.get_track(index[1]))
     realIndex2 = None
     if index_end is None:
@@ -28,25 +27,56 @@ def search_by_album_name(repository=repo, string_to_search='free', shallow_searc
     else:
         realIndex2 = tracks_sorted_by_albums.index(repository.get_track(index_end[1]))
     print(realIndex1,realIndex2)
-    print('_______________________')
     for i in range(realIndex1,realIndex2):
         #print(index, index_end)
         if tracks_sorted_by_albums[i].album is not None:
             if tracks_sorted_by_albums[i].album.title[:len(string_to_search)].upper() == string_to_search.upper():
                 results_with_search_term_at_front.append(tracks_sorted_by_albums[i])
-                # print(tracks_sorted_by_albums[i].album.title[:len(string_to_search)].upper())
-    # print(results_with_search_term_at_front)
-    # for i in results_with_search_term_at_front:
-    #     print("OOOO\n",i.album.title,'')
     if shallow_search==True:
         return results_with_search_term_at_front
-
     for i in range(0, len(tracks_sorted_by_albums)):
         if tracks_sorted_by_albums[i].album is not None:
             if string_to_search.upper() in tracks_sorted_by_albums[i].album.title.upper():
                 if tracks_sorted_by_albums[i] not in results_with_search_term_at_front:
                     result_with_search_term_in_middle_or_end.append(tracks_sorted_by_albums[i])
-
-    # for i in result_with_search_term_in_middle_or_end:
-    #     print(i.album.title)
     return results_with_search_term_at_front, result_with_search_term_in_middle_or_end
+
+
+def search_by_track_name(repository=repo, string_to_search='free', shallow_search=False):
+    tracks_by_track_name = repository.tracks_t
+    bookmarks = list(repository.create_bookmarks(tracks_by_track_name, 0))
+    # [item,item.........]
+    results_with_search_term_at_front = []
+    result_with_search_term_in_middle_or_end = []
+    index = None
+    index_end = None
+    for i in range(0, len(bookmarks)):
+        if bookmarks[i][0] == string_to_search[0].upper():
+            index = bookmarks[i]
+            if bookmarks[i] != bookmarks[-1]:
+                index_end = bookmarks[i+1]
+
+            break
+    print(index, index_end)
+    realIndex1 = tracks_by_track_name.index(repository.get_track(index[1]))
+    realIndex2 = None
+    if index_end is None:
+        realIndex2 = len(tracks_by_track_name)
+    else:
+        realIndex2 = tracks_by_track_name.index(repository.get_track(index_end[1]))
+    print(realIndex1,realIndex2)
+    for i in range(realIndex1,realIndex2):
+        #print(index, index_end)
+        if tracks_by_track_name[i].title is not None:
+            if tracks_by_track_name[i].title[:len(string_to_search)].upper() == string_to_search.upper():
+                results_with_search_term_at_front.append(tracks_by_track_name[i])
+    if shallow_search==True:
+        return results_with_search_term_at_front
+    for i in range(0, len(tracks_by_track_name)):
+        if tracks_by_track_name[i].title is not None:
+            if string_to_search.upper() in tracks_by_track_name[i].title.upper():
+                if tracks_by_track_name[i] not in results_with_search_term_at_front:
+                    result_with_search_term_in_middle_or_end.append(tracks_by_track_name[i])
+    return results_with_search_term_at_front, result_with_search_term_in_middle_or_end
+
+
